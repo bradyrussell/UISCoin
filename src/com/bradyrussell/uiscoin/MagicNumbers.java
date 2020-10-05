bda6e926522335466fd884a9c8947273d00284df
BradyRussell <31458670+bradyrussell@users.noreply.github.com>
2020-10-05 09:07:13 -0500

    increase MaxTransactionSize and MaxBlockSize to be 5 and 10 times higher respectively

--- src/com/bradyrussell/uiscoin/MagicNumbers.java
+++ src/com/bradyrussell/uiscoin/MagicNumbers.java
@@ -3,8 +3,8 @@ package com.bradyrussell.uiscoin;
 public enum MagicNumbers {
     NodeP2PPort(25920),
     NodeP2PTimeout(300)   ,
-    MaxTransactionSize(1024),
-    MaxBlockSize(1024*1024),
+    MaxTransactionSize(1024*5),
+    MaxBlockSize(1024*1024*10),
     MinSatPerByte(1),
     MaxLockingScriptLength(256),
     MaxUnlockingScriptLength(256),
